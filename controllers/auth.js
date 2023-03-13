8b4546adbef591911cd2c907ef1c501224f2aa70
rabin245 <drarabin@gmail.com>
2023-03-13 14:08:25 +0545

    link metamask account with user

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,33 +5,44 @@ const jwt = require("jsonwebtoken");
 const users = [
   {
     id: 1,
-    username: "John Doe",
-    email: "test@gmail.com",
-    hashedPassword: "password",
-    isAdmin: false,
+    username: "admin",
+    email: "admin@gmail.com",
+    // hashedPassword: "password",
+    password: "password",
+    isAdmin: true,
+    address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
   },
   {
     id: 2,
-    username: "admin",
-    email: "admin@gmail.com",
-    hashedPassword: "password",
-    isAdmin: true,
+    username: "John Doe",
+    email: "test@gmail.com",
+    // hashedPassword: "password",
+    password: "password",
+    isAdmin: false,
+    address: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
   },
 ];
 
 const register = async (req, res) => {
   try {
-    const { id, username, email, password } = req.body;
+    const { id, username, email, password, address } = req.body;
 
-    const checkUser = users.find((user) => user.email === email);
+    const checkEmailUser = users.find((user) => user.email === email);
 
-    if (checkUser)
+    if (checkEmailUser)
       return res.status(400).json({ message: "Email is already in use" });
+
+    const checkAddressUser = users.find((user) => user.address == address);
+
+    if (checkAddressUser)
+      return res
+        .status(400)
+        .json({ message: "Metamask account already in use" });
 
     // const salt = await bcrypt.genSalt(10);
     // const hashedPassword = await bcrypt.hash(password, salt);
-    // const user = { id, username, email, hashedPassword, isAdmin: false };
-    const user = { id, username, email, password, isAdmin: false };
+    // const user = { id, username, email, hashedPassword, address, isAdmin: false };
+    const user = { id, username, email, password, address, isAdmin: false };
 
     users.push(user);
 
@@ -46,12 +57,17 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    const { email, password: pwd } = req.body;
+    const { email, password: pwd, address } = req.body;
 
     const checkUser = users.find((user) => user.email === email);
 
     if (!checkUser)
       return res.status(400).json({ message: "User does not exist" });
+
+    if (checkUser.address !== address)
+      return res
+        .status(400)
+        .json({ message: "Use the metamask account linked with the email" });
 
     // const validPassword = bcrypt.compareSync(pwd, checkUser.hashedPassword);
 
@@ -65,7 +81,7 @@ const login = async (req, res) => {
       { ...userWithoutPassword },
       process.env.JWT_PRIVATE_KEY,
       {
-        expiresIn: 86400, // 24 hours
+        expiresIn: 900, // 15 min
       }
     );
 
